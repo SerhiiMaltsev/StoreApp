4f0e6fb5bd9ae5dda37b69807932b30249c609ef
SerhiiMaltsev <sm5zj@virginia.edu>
2022-06-07 16:45:03 -0400

    a few changes

--- frontend/src/Components/Home/SideBar.js
+++ frontend/src/Components/Home/SideBar.js
@@ -2,11 +2,33 @@ import * as React from 'react';
 import {Box, Grid, Paper, Drawer, AppBar, CssBaseline, Toolbar, List, Typography, Divider, Button, TextField} from '@mui/material';
 import "./SideBar.css"
 import Item from "./Item.js"
+import {useState, useEffect} from 'react';
 
 const drawerWidth = "35vh";
 
 export default function ClippedDrawer() {
-  const listOfProducts = [{name: "product1"}, {name: "product2"}, {name: "product3"}, {name: "product4"}, {name: "product5"}, {name: "product6"}, {name: "product7"}, {name: "product8"}, {name: "product9"}, {name: "product10"}, {name: "product11"}]
+  var listOfProducts = [{name: "product1"}, {name: "product2"}, {name: "product3"}, {name: "product4"}, {name: "product5"}, {name: "product6"}, {name: "product7"}, {name: "product8"}, {name: "product9"}, {name: "product10"}, {name: "product11"}]
+  var shownProducts = [{name: "product1"}, {name: "product2"}, {name: "product3"}, {name: "product4"}, {name: "product5"}, {name: "product6"}, {name: "product7"}, {name: "product8"}, {name: "product9"}, {name: "product10"}, {name: "product11"}]
+  const [products, setProducts] = useState([]);
+  const [searchInput, setSearchInput] = useState();
+
+  function search(){
+    shownProducts = []
+    for (let i = 0; i < listOfProducts.length; i++) {
+      if (listOfProducts[i].name.includes(searchInput)){
+        shownProducts.push(listOfProducts[i])
+      }
+    setProducts(shownProducts)
+    }
+  }
+
+  useEffect(() => {
+    setProducts(shownProducts)
+  }, [])
+
+  function reset(){
+    setProducts(listOfProducts)
+  }
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -36,9 +58,10 @@ export default function ClippedDrawer() {
         <Toolbar />
         <Box sx={{ overflow: 'auto' }}>
           <div className="SearchElems">
-            <TextField id="Search-bar" label="Search" variant="outlined"/>
+            <TextField id="Search-bar" label="Search" variant="outlined" onChange={(e) => {setSearchInput(e.target.value)}}/>
             <div className="SearchButton">
-              <Button variant="contained">Search</Button>
+              <Button variant="contained" onClick={search}>Search</Button>
+              <Button variant="contained" onClick={reset}>Reset</Button>
             </div>
           </div>
         </Box>
@@ -47,9 +70,9 @@ export default function ClippedDrawer() {
       <Box component="main" >
         <Toolbar />
         <Grid className="Products" container spacing={10}>
-          {Object.keys(listOfProducts).map((keyName, i) => (
+          {Object.keys(products).map((keyName, i) => (
             <Grid className="Product" item xs={2.5}>
-              <Item product={listOfProducts[i]}/>
+              <Item product={products[i]}/>
             </Grid>
           ))}
         </Grid>
